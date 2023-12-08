9b83f78471a9aca779a8b0a1497d7bf5dff1297d
salusKarabu <k.mariasalus@gmail.com>
2023-12-08 20:52:46 +0300

    make conflict

--- src/Pages/about.js
+++ src/Pages/about.js
@@ -13,7 +13,6 @@ const About = () => {
       <h2>About Christmas</h2>
       <p>
         Christmas is a time for joy, celebration, and giving. It marks the birth of Jesus Christ
-        and is celebrated by people around the world with various traditions and customs.
       </p>
     </div>
     </div>
