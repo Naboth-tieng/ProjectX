ad4acddaccea5a5f6a2a6c8bb9bedf9816602633
Naboth Tieng <nabothtieng@gmail.com>
2023-12-08 20:50:23 +0300

    Trying to make a conflict

--- src/Pages/about.js
+++ src/Pages/about.js
@@ -12,7 +12,7 @@ const About = () => {
     <div>
       <h2>About Christmas</h2>
       <p>
-        Christmas is a time for joy, celebration, and giving. It marks the birth of Jesus Christ
+        Christmas is a time for joy, celebration, and giving love and other goodies out to your loved ones. It marks the birth of Jesus Christ
         and is celebrated by people around the world with various traditions and customs.
       </p>
     </div>
